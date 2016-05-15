844d7fd1afa4e4ab9e6bff5af45501844561bbe3
Eli <elimelec.burghelea@gmail.com>
2016-05-15 22:56:39 +0300

    Open Microcode

--- gui.rkt
+++ gui.rkt
@@ -9,9 +9,6 @@
     (define source (file->lines path))
     (define asembled (compile-asm path))
 
-    (define source-code-panel (new horizontal-panel%
-                                   [parent frame]
-                                   [style (list 'border)]))
     (define source-code (new list-box%
                              [label #f]
                              [parent source-code-panel]
@@ -28,12 +25,49 @@
                                [columns '("Asembled code")]))
     source-code))
 
+(define (open-microcode button event)
+  (define path (get-file))
+  (when path
+    (define path-string (path->string path))
+    (define microprogram-bin (file->lines path-string))
+    (define microprogram-text (file->lines (string-replace path-string "bin" "txt")))
+
+    (define source-code (new list-box%
+                             [label #f]
+                             [parent microcode-panel]
+                             [choices microprogram-text]
+                             [style (list 'single
+                                          'column-headers)]
+                             [columns '("Text Microcode")]))
+    (define asembled-code (new list-box%
+                               [label #f]
+                               [parent microcode-panel]
+                               [choices microprogram-bin]
+                               [style (list 'single
+                                            'column-headers)]
+                               [columns '("Binary Microcode")]))
+    source-code))
+
 (define frame (new frame% [label "Simelitor"]))
 
-(define button (new button%
-                    [parent frame]
-                    [label "Open"]
+(define buttons-panel (new horizontal-panel%
+                           [parent frame]
+                           [style (list 'border)]))
+(define source-code-panel (new horizontal-panel%
+                                   [parent frame]
+                                   [style (list 'border)]))
+(define microcode-panel (new horizontal-panel%
+                                   [parent frame]
+                                   [style (list 'border)]))
+
+(define open-source-button (new button%
+                    [parent buttons-panel]
+                    [label "Open Source"]
                     [callback open-file]))
+(define open-microcode-button (new button%
+                    [parent buttons-panel]
+                    [label "Open Microcode"]
+                    [callback open-microcode]))
 
 (send frame show #t)
 

b5bdc605bf22240f98ce267fe33d73f014f43308
Eli <elimelec.burghelea@gmail.com>
2016-05-19 01:48:36 +0300

    Set default length for bin to 16

--- bin.rkt
+++ bin.rkt
@@ -2,8 +2,9 @@
 
 (provide bin dec)
 
-(define (bin n l)
-  (~a (number->string n 2) #:min-width l #:align 'right #:left-pad-string "0"))
+(define bin
+  (lambda (n [l 16])
+    (~a (number->string n 2) #:min-width l #:align 'right #:left-pad-string "0")))
 
 (define (dec n)
   (string->number n 2))

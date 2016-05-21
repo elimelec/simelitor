93c16756128817a31f37a89e3c4844d59ea8aa17
Eli <elimelec.burghelea@gmail.com>
2016-05-21 22:11:04 +0300

    Add bitstring-not

--- bin.rkt
+++ bin.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(provide bin dec)
+(provide (all-defined-out))
 
 (define bin
   (lambda (n [l 16])
@@ -8,3 +8,7 @@
 
 (define (dec n)
   (string->number n 2))
+
+(define (bitstring-not s)
+  (regexp-replace* #px"[01]" s
+                   (lambda (s) (if (string=? s "0") "1" "0"))))

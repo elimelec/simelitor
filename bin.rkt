36cd2bff49842630516bd53c586b80090fffcab7
Eli <elimelec.burghelea@gmail.com>
2016-05-24 12:17:21 +0300

    Add dec function for signed binary numbers

--- bin.rkt
+++ bin.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require threading)
+
 (provide (all-defined-out))
 
 (define bin
@@ -18,6 +20,13 @@
 
 (define (dec n)
   (string->number n 2))
+
+(define (sdec n)
+  (let ([sign (substring n 0 1)]
+        [number (substring n 1)])
+    (match sign
+      ["0" (dec number)]
+      ["1" (~> number bitstring-not dec add1 -)])))
 
 (define (bitstring-not s)
   (regexp-replace* #px"[01]" s

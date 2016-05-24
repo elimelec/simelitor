33ae5c53f0396152ca8848e0a0dc2cc6e5c67449
Eli <elimelec.burghelea@gmail.com>
2016-05-24 23:56:45 +0300

    Update example

--- test.s
+++ test.s
@@ -1,29 +1,25 @@
-add r1, 65535
-add r1, r2 ;ia ui ba si comenturi
-sub r2, r5
-add r1, r4 ;comment
-inc r2
-inc r3
-clc
-clv
-nop
+mov r1, 100
+mov r2, -100
+mov r3, -10
+xor r3, r1
+neg r10
+clr r10
+dec r10
+and r10, r3
+or r10, r3
+xor r10, r3
+add r1, r10
+inc r1
+add r3, r2
 inc r4
-clc
-clv
-clz
-cls
-ccc
-sec
-sev
-sez
-ses
-scc
-nop
-ret
-reti
-halt
-wait
-push-pc
-pop-pc
-push-flag
-pop-flag
+inc r4
+inc r4
+neg r4
+br 6
+add r1, 0
+add r1, 0
+add r1, 0
+br -24
+add r1, 0
+add r1, 0
+add r1, 0
